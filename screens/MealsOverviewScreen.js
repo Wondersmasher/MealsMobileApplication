4d422f90ac89f0665d530e9c5058101c59fdca7d
wondersmasher <wonderfulakinniyi@gmai.com>
2023-07-23 20:42:10 +0100

    search functionality for single meal group

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
-
+import { MEALS } from "../data/dummy-data";
+// CATEGORIES
 const MealsOverviewScreen = ({ navigation, route }) => {
   const catId = route.params.categoryId;
+  const displayMeal = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
   return (
     <View>
       <Text>Meals Overview Screen {catId}</Text>
